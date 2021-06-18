760e6967c2599c4928c1db16d22e49c63b4f3168
Flloki5 <rozli@op.pl>
2021-06-18 12:08:36 +0200

    Copnfigure postgresDB and hibernate

---
+++ src/main/java/com/currencyapp/configuration/PostgresConfig.java
@@ -0,0 +1,24 @@
+package com.currencyapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.currencyapp.configuration")
+public class PostgresConfig {
+
+    @Bean
+    public DataSource postgresDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost/currencydb");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+
+        return  dataSource;
+    }
+}
--- src/main/java/com/currencyapp/models/Currency.java
+++ src/main/java/com/currencyapp/models/Currency.java
@@ -1,16 +1,33 @@
 package com.currencyapp.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Currency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
     private String shortcutName;
     private int amount;
 
-
     public Currency(String name, String shortcutName, int amount) {
         this.name = name;
         this.shortcutName = shortcutName;
         this.amount = amount;
+    }
+
+    public Currency() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
